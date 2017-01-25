4e10e9033e53539a46f215e8f3ed85ccfcc3e777
Andreas Östlund <aost@Andreass-MacBook-Pro.local>
2017-01-25 13:05:13 +0100

    added comments and cleaned comics.js

--- js/comics.js
+++ js/comics.js
@@ -1,3 +1,7 @@
+/*----------------------------------------------
+ Load eventual search parameters from the url
+ and parse them into parameters fit for the API
+----------------------------------------------*/
 let searchType = getUrlProperties(location.search).type || 'characters';
 let searchString = getUrlProperties(location.search).search || '';
 searchString = searchString ? 
@@ -5,39 +9,36 @@ searchString = searchString ?
     `titleStartsWith=${searchString}&` : 
     `nameStartsWith=${searchString}&` : 
   '';
+
+/*------------------------------------
+ Load data, initialize content holder
+ with data and add infinite scroll
+------------------------------------*/ 
 let comicsData;
 let comicsArray = [];
 let state = {};
-$.get(`https://gateway.marvel.com:443/v1/public/${searchType}?${searchString}apikey=${apiKey}`, result => {
-  state[searchType] = result.data.results;
-  $('.content-holder').append(`<h2>${searchType}</h2>
-     <div class="card-container" id="${searchType}" style="height:auto"></div>`
-  );
-  updateHolder(searchType, 0);
-  $('.spinner').remove();
-  addInfiniteScroll(getMoreComics);
+$(() => {
+  $.get(`https://gateway.marvel.com:443/v1/public/${searchType}?${searchString}apikey=${apiKey}`, result => {
+    state[searchType] = result.data.results;
+    $('.content-holder').append(`<h2>${searchType}</h2>
+      <div class="card-container" id="${searchType}" style="height:auto"></div>`
+    );
+    updateHolder(searchType, 0);
+    $('.spinner').remove();
+    addInfiniteScroll(getMoreComics);
+  });
 });
 
+/*--------------------------------
+ Function for getting more comics
+--------------------------------*/
 const getMoreComics = () => {
   const offset = state[searchType].length;
-  $('body').append('<div class="spinner" />');
+  $('body').append('<div class="spinner"/>');
   $.get(`https://gateway.marvel.com:443/v1/public/${searchType}?${searchString}offset=${offset}&apikey=${apiKey}`, comics => {
     state[searchType] = state[searchType].concat(comics.data.results);
     updateHolder(searchType, offset);
     $('.spinner').remove();
     addInfiniteScroll(getMoreComics);
   });
-};
-
-const drawThumbnails = (comics, mountPoint) => {
-  const base = $('<div class="content-holder" />');
-  for (let i = 0; i < comics.length; i++) {
-    if (!(comics[i].thumbnail.path === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available')) {
-      base.append(`<a href="./views/character.html?id=${comics[i].id  
-                  }"><div class="card"><img src="${comics[i].thumbnail.path}.${ 
-                  comics[i].thumbnail.extension}">${comics[i].name}</div></a>`);
-    }
-  }
-  $(mountPoint).empty();
-  $(mountPoint).append(base);
 };
